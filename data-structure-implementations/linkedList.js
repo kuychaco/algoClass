9f560ed107dde6549de6c1f139b45f73da8de5e0
Katrina Uychaco <kuychaco@gmail.com>
2015-09-08 12:54:52 -0700

    Add instructions and tests

--- data-structure-implementations/linkedList.js
+++ data-structure-implementations/linkedList.js
@@ -8,17 +8,21 @@ Each node is composed of data and a reference/link to the next node.
 
 *** Operations:
 
-** Node methods:
+** Part 1
 
-refNode.insertAfter(value)
-=> new value
+myList.forEach(callbackFn)
+invoke callback function with the value of each node
+
+myList.print()
+=> string with all values in list (ex: '0, 1, 2, 3')
+
+myList.insertAfter(refNode, value)
+=> value of new node
 insert new node associated with value passed in after refNode
 
-refNode.removeAfter()
+myList.removeAfter(refNode)
 => value of removed node
-remove node after the reference node
-
-** List methods:
+remove node after the refNode
 
 myList.insertHead(value)
 => value associated with new head
@@ -28,13 +32,23 @@ myList.removeHead()
 => value of removed head node
 remove the head node of the linked list
 
+myList.findNode(value)
+=> first node that has a value matching what was passed in
+
+
 * Optimization:
 Say we have a linked list that has 100 items and we want to add an item to the very end. How would you do that with your current implementation? How can you modify the data structure to add an item to the end in constant time?
 
-myList.append(value)
+myList.appendToTail(value)
 => value of tail node
 add a new tail node at the end of the list with the associated value passed in
 
+myList.removeTail()
+=> value of removed tail node
+remove the tail node from the list
+
+
+** Part 2
 
 Now let's think about creating insertBefore and removeBefore methods for the nodes in our list. Can you think of an efficient way to do so?
 
@@ -44,21 +58,14 @@ How can we modify our data structures (Node and Linked List classes) so that we 
 
 Once you've come up with a plan, implement the following methods.
 
-refNode.insertBefore(value)
+myList.insertBefore(refNode, value)
 => value of new node inserted
 insert new node with associated value before refNode
 
-refNode.removeBefore()
+myList.removeBefore(refNode)
 => value of removed node
-remove node before the reference node passed in
+remove node before the refNode passed in
 
-myList.appendTail(value)
-=> value of new tail
-append new node at the end of the list with the associated value
-
-myList.removeTail()
-=> value of removed tail node
-remove the tail node of the linked list
 
 
 *** Nightmare mode:
@@ -96,14 +103,6 @@ LinkedList.prototype.print = function() {
     result.push(value);
   });
   return result.join(', ');
-};
-
-LinkedList.prototype.findNode = function(value) {
-  var node = this.head;
-  while (node) {
-    if (node.value === value) return node;
-  }
-  return 'No node with value: ' + value + ' found.';
 };
 
 LinkedList.prototype.insertAfter = function(node, value) {
@@ -146,6 +145,15 @@ LinkedList.prototype.removeHead = function() {
   return oldHead.value;
 }
 
+LinkedList.prototype.findNode = function(value) {
+  var node = this.head;
+  while (node) {
+    if (node.value === value) return node;
+    node = node.next;
+  }
+  return 'No node with value: ' + value + ' found.';
+};
+
 LinkedList.prototype.appendToTail = function(value) {
   var newTail = new Node(value);
 
@@ -160,7 +168,6 @@ LinkedList.prototype.appendToTail = function(value) {
 };
 
 
-var myEmptyList = new LinkedList();
 var myList = new LinkedList(0);
 
 console.log(myList.print(), 'should be 0');
@@ -177,6 +184,12 @@ console.log(myList.print(), 'should be -1, 0, 2, 3');
 console.log(myList.removeHead(), 'should be -1');
 console.log(myList.print(), 'should be 0, 2, 3');
 console.log(myList.appendToTail(4), 'should be 4');
+console.log(myList.print(), 'should be 0, 2, 3, 4');
+console.log(myList.findNode(0) === myList.head, 'should be true');
+console.log(myList.findNode(3) === myList.head.next.next, 'should be true');
+myList.insertAfter(myList.findNode(2), 2.5);
+console.log(myList.print(), 'should be 0, 2, 2.5, 3, 4');
+myList.removeAfter(myList.findNode(2));
 console.log(myList.print(), 'should be 0, 2, 3, 4');
 
 
