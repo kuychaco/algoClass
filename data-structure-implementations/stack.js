567a8e6d8def3040dc814851390b16164c6cf46d
Katrina Uychaco <kuychaco@gmail.com>
2015-09-08 00:09:48 -0700

    Remove _index property

--- data-structure-implementations/stack.js
+++ data-structure-implementations/stack.js
@@ -52,32 +52,31 @@ What's the time complexity?
 function Stack(capacity) {
   this._capacity = capacity;
   this._storage = {};
-  this._index = 0;
   this._count = 0;
 }
 
 // O(1)
 Stack.prototype.push = function(value) {
   if (this._count < this._capacity) {
-    this._storage[this._index++] = value;
-    return ++this._count;
+    this._storage[this._count++] = value;
+    return this._count;
   }
   return 'Max capacity already reached. Remove element before adding a new one.';
 };
 
 // O(1)
 Stack.prototype.pop = function() {
-  var value = this._storage[--this._index];
-  delete this._storage[this._index];
-  if (this._count > 0) {
-    this._count--;
+  var value = this._storage[--this._count];
+  delete this._storage[this._count];
+  if (this._count < 0) {
+    this._count = 0;
   }
   return value;
 };
 
 // O(1)
 Stack.prototype.peek = function() {
-  return this._storage[this._index-1];
+  return this._storage[this._count-1];
 }
 
 // O(1)
