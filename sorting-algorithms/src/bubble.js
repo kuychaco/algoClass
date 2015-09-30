fce1423a257722a2e2e38c2f4646664c3dfef480
Katrina Uychaco <kuychaco@gmail.com>
2015-09-30 12:44:20 -0700

    Remove solutions

--- sorting-algorithms/src/bubble.js
+++ sorting-algorithms/src/bubble.js
@@ -19,29 +19,3 @@ Variants:
 (https://en.wikipedia.org/wiki/Cocktail_sort)
 
 */
-
-
-var bubbleSort = function(array) {
-  // while wall > 0
-  var wall = array.length; // first sorted element
-  // iterate through array up to wall
-  while (wall >= 0) {
-    // if next value < current, swap
-    for (var i=0; i<wall; i++) {
-      if (array[i] > array[i+1]) {
-        array = swap(array, i, i+1);
-      }
-    }
-    wall--;
-  }
-  return array;
-};
-
-function swap(arr, i1, i2) {
-  // Bitwise swap
-  // Note: only works with integer elements
-  arr[i1] = arr[i1]^arr[i2];
-  arr[i2] = arr[i1]^arr[i2];
-  arr[i1] = arr[i1]^arr[i2];
-  return arr;
-}
--- sorting-algorithms/src/insertion.js
+++ sorting-algorithms/src/insertion.js
@@ -29,33 +29,3 @@ now repeat for next unsorted element
 (https://en.wikipedia.org/wiki/Shellsort)
 
 */
-
-var insertionSort = function(array, comparator) {
-  comparator = comparator || defaultComparator;
-
-  // start at index 1 as sublist of array[0] is already sorted
-  for (var index=1; index<array.length; index++) {
-    var value = array[index];
-    var compareIndex = index-1;
-    // swap with left element while < value
-    while (compareIndex > -1 && comparator(array[compareIndex], value) > 0) {
-      array = swap(array, compareIndex, index);
-      index = compareIndex;
-      compareIndex--;
-    }
-  }
-  return array;
-};
-
-function defaultComparator(a,b) {
-  if (a < b) return -1; // a comes first
-  else if (a > b) return 1; // b comes first
-  return 0;
-};
-
-function swap (arr, i1, i2) {
-  var temp = arr[i1];
-  arr[i1] = arr[i2];
-  arr[i2] = temp;
-  return arr;
-}
