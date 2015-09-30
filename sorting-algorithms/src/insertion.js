081f778779cfd0afe011b44d2c1924cd3e23cef4
Katrina Uychaco <kuychaco@gmail.com>
2015-09-30 12:04:29 -0700

    Implement solution

--- sorting-algorithms/src/insertion.js
+++ sorting-algorithms/src/insertion.js
@@ -7,13 +7,21 @@ Iterate over array and grow a sorted array behind current element.
 
 For each position, compare value of element with previous elements and swap positions if previous element is larger.
 
+example:
+[ 3 4 5|1 2 6 ]
+ sorted|unsorted
+swaps:
+[ 3 4 1 5|2 6 ]
+[ 3 1 4 5|2 6 ]
+[ 1 3 4 5|2 6 ]
+now repeat for next unsorted element
+
 *** Exercises
 
 - Implement insertion sort for array of numbers
 - Identify time complexity
 
 - Modify function to take comparator function. specify default if not provided (check out native Array.sort comparator function for reference)
-
 - Use your comparator function to verify that your sort is stable by taking input: [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
 
 *** Extra credit
