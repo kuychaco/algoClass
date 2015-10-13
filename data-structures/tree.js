305848ccbe53b277edec1c7209611c46c969b54e
Katrina Uychaco <kuychaco@gmail.com>
2015-10-12 18:10:32 -0700

    Add exercise for prefix tree

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -36,6 +36,8 @@ Invoke the callback for every node in a breadth-first order
 *** Nightmare mode:
 Given treeA and treeB, check if treeB is a subtree of treeA (meaning that there exists a node n in treeA such that the subtree of n is identical to treeB).
 
+Given a dictionary, create a prefix tree (commonly known as a trie)
+https://en.wikipedia.org/wiki/Trie
 */
 
 // N-ary Tree (any number of children)
