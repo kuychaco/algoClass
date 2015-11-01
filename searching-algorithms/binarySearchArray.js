81d2c3b58f5c96cb31cb2b6624258ecea09a6190
Katrina Uychaco <kuychaco@gmail.com>
2015-10-31 23:00:34 -0700

    Add binary search in array

---
+++ searching-algorithms/binarySearchArray.js
@@ -0,0 +1,14 @@
+/*
+BINARY SEARCH ARRAY
+
+*** Description
+
+Given a sorted array and a value, determine if the value is in the array using the binary search (divide and conquer) method.
+
+*** Exercises
+
+Write a function that takes a sorted array and a value and returns the index of the value in the array. Return null if the value is not found in the array. What is the time complexity?
+
+Extra credit: Implement the function both iteratively and recursively.
+
+*/
