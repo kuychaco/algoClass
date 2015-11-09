b22d12da59debd240444ee09d5279c1f23fc6243
Katrina Uychaco <kuychaco@gmail.com>
2015-11-08 21:29:22 -0800

    Implement traversal methods

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -18,31 +18,79 @@ graph = {
 }
 
 Constraints:
-This graph implementation is undirected and can have unconnected nodes.
+This graph implementation is undirected and can have unconnected nodes. The nodes are represented by unique primitive values.
 
 *** Operations:
+graph.addNode(value) // value must be a primitive
+=> undefined
+Add node to graph
 
+graph.removeNode(value)
+=> undefined
+Remove node from graph
 
+graph.contains(value)
+=> true/false
+Returns true if value is found in graph, false otherwise
+
+graph.addEdge(value1, value2)
+=> undefined
+Create connection between two nodes if they're both present in the graph
+
+graph.removeEdge(value1, value2)
+=> undefined
+Remove connection between two nodes
+
+graph.hasEdge(value1, value2)
+=> true/false
+Returns true if edge exists, false otherwise
+
+graph.forEach(callback)
+=> undefined
+Traverse the graph and invoke the passed callback once for each node. The callback function receives the following for each node: node value, node Neighbors, all nodes.
 
 *** Nightmare mode:
 
+Implement traversal methods for depth-first and breadth-first traversal. The methods take a starting node and a callback that gets invoked for each node. The callback should receive two arguments: the node value and the distance (number of edges that separate the node from the starting node). See example usage below.
+
+graph.traverseDepthFirst(value1, callback)
+=> undefined
+Starting at the node with the value passed in, traverse the graph and invoke the callback for each node in a depth-first fashion.
+
+graph.traverseBreadthFirst(value, callback)
+=> undefined
+Starting at the node with the value passed in, traverse the graph and invoke the callback for each node in a breadth-first fashion.
+
+Example Usage:
+1---2---3---5
+ \ /
+  4
+{
+  '1': [ 2, 4 ],
+  '2': [ 1, 3, 4 ],
+  '3': [ 2, 5 ],
+  '4': [ 1, 2 ],
+  '5': [ 3 ]
+}
+
+var traverseDF = [];
+graph.traverseDepthFirst(1, function(val, distance) { traverseDF.push([val, distance]) });
+traverseDF should be [ [ 1, 0 ], [ 2, 1 ], [ 3, 2 ], [ 5, 3 ], [ 4, 2 ] ]
+
+var traverseBF = [];
+graph.traverseBreadthFirst(1, function(val, distance) { traverseBF.push([val, distance]) });
+traverseBF should be [ [ 1, 0 ], [ 2, 1 ], [ 4, 1 ], [ 3, 2 ], [ 5, 3 ] ]
+
+
+*** Exercises:
+
+Given a directed graph and two nodes in the graph, write a function that indicates whether there is a route between the two nodes. Bonus: rather than returning a boolean, have your function return the shortest distance between the two nodes (the number of edges that separate them).
 
 */
 
 function Graph () {
   this._nodes = {};
 }
-
-/*
-This implementation represents a graph with an adjacency list.
-Example:
-{
-  1: [2, 4],
-  2: [1, 3, 4],
-  3: [2],
-  4: [1, 2]
-}
-*/
 
 Graph.prototype.addNode = function(value) {
   if (value === undefined) return;
@@ -51,9 +99,9 @@ Graph.prototype.addNode = function(value) {
 
 Graph.prototype.removeNode = function(value) {
   this._nodes[value].forEach(function(neighbor) {
-    var neighborEdges = this._nodes[neighbor];
-    var index = neighborEdges.indexOf(value);
-    neighborEdges.splice(index, 1);
+    var neighborsNeighbors = this._nodes[neighbor];
+    var index = neighborsNeighbors.indexOf(value);
+    neighborsNeighbors.splice(index, 1);
   })
   delete this._nodes[value];
 };
@@ -86,6 +134,38 @@ Graph.prototype.forEach = function(fn) {
   }
 };
 
+Graph.prototype.traverseDepthFirst = function(value, fn, visited, distance) {
+  if (!this._nodes[value] || typeof fn !== 'function') return 'Invalid value or function';
+  visited = visited || {};
+  distance = distance || 0;
+  fn(value, distance);
+  visited[value] = true;
+  this._nodes[value].forEach(function(neighbor) {
+    if (visited[neighbor]) return;
+    this.traverseDepthFirst(neighbor, fn, visited, distance+1);
+  }, this);
+};
+
+Graph.prototype.traverseBreadthFirst = function(value, fn) {
+  if (!this._nodes[value] || typeof fn !== 'function') return 'Invalid value or function';
+  var visited = {};
+  var queue = [value];
+  visited[value] = 0;
+  while (queue.length) {
+    var node = queue.shift();
+    fn(node, visited[node]);
+    var neighbors = this._nodes[node].filter(function(neighbor) {
+      if (visited[neighbor] === undefined) {
+        visited[neighbor] = visited[node]+1;
+        return true;
+      }
+    });
+    queue = queue.concat(neighbors);
+  }
+};
+
+
+
 var graph = new Graph();
 
 graph.addNode(1);
@@ -111,3 +191,12 @@ console.log(graph.hasEdge(1,3), 'should be false');
 graph.forEach(function(node, neighbors) {
   console.log(node, 'has neighbors:', neighbors);
 });
+graph.addNode(5);
+graph.addEdge(3,5);
+console.log(graph._nodes);
+var traverseDF = [];
+graph.traverseDepthFirst(1, function(val, dist) { traverseDF.push([val, dist]) });
+console.log(traverseDF, 'should be [ [ 1, 0 ], [ 2, 1 ], [ 3, 2 ], [ 5, 3 ], [ 4, 2 ] ]');
+var traverseBF = [];
+graph.traverseBreadthFirst(1, function(val, dist) { traverseBF.push([val, dist]) });
+console.log(traverseBF, 'should be [ [ 1, 0 ], [ 2, 1 ], [ 4, 1 ], [ 3, 2 ], [ 5, 3 ] ]');
