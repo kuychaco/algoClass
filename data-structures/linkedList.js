31556d94b9378f0c4ccb04b7c4bd3b4b5e0b9ba1
Katrina Uychaco <kuychaco@gmail.com>
2015-11-08 17:50:55 -0800

    Return nodes rather than values

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -16,35 +16,35 @@ invoke callback function with the value of each node
 myList.print()
 => string with all values in list (ex: '0, 1, 2, 3')
 
-myList.insertAfter(refNode, value) 
-=> value of new node
+myList.insertAfter(refNode, value)
+=> new node
 insert new node associated with value passed in after refNode
 
-myList.removeAfter(refNode) 
-=> value of removed node 
+myList.removeAfter(refNode)
+=> removed node
 remove node after the refNode
 
 myList.insertHead(value)
-=> value associated with new head 
+=> new head
 insert new head node at the beginning of the list with the value passed in
 
 myList.removeHead()
-=> value of removed head node
+=> removed head node
 remove the head node of the linked list
 
 myList.findNode(value)
 => first node that has a value matching what was passed in
 
 
-* Optimization: 
+* Optimization:
 Say we have a linked list that has 100 items and we want to add an item to the very end. How would you do that with your current implementation? How can you modify the data structure to add an item to the end in constant time?
 
 myList.appendToTail(value)
-=> value of tail node
+=> new tail node
 add a new tail node at the end of the list with the associated value passed in
 
 myList.removeTail()
-=> value of removed tail node
+=> removed tail node
 remove the tail node from the list
 
 
@@ -52,20 +52,19 @@ remove the tail node from the list
 
 Now let's think about creating insertBefore and removeBefore methods for the nodes in our list. Can you think of an efficient way to do so?
 
-Think about time complexity. What would it be for your current implementation of a linked list? 
+Think about time complexity. What would it be for your current implementation of a linked list?
 
 How can we modify our data structures (Node and Linked List classes) so that we can make these O(1) operations?
 
 Once you've come up with a plan, implement the following methods.
 
-myList.insertBefore(refNode, value) 
-=> value of new node inserted
+myList.insertBefore(refNode, value)
+=> new node inserted
 insert new node with associated value before refNode
 
-myList.removeBefore(refNode) 
-=> value of removed node
+myList.removeBefore(refNode)
+=> removed node
 remove node before the refNode passed in
-
 
 
 *** Extra Credit:
@@ -94,27 +93,27 @@ function LinkedList(headValue) {
 LinkedList.prototype.forEach = function(callback) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.print = function() {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.insertAfter = function(node, value) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.removeAfter = function(node) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.insertHead = function(value) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.removeHead = function() {
   // implement me...
@@ -123,12 +122,12 @@ LinkedList.prototype.removeHead = function() {
 LinkedList.prototype.findNode = function(value) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.appendToTail = function(value) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 
 // PART 2:
@@ -136,12 +135,12 @@ LinkedList.prototype.appendToTail = function(value) {
 LinkedList.prototype.insertBefore = function(node, value) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 LinkedList.prototype.removeBefore = function(node) {
   // implement me...
 };
-// Time complexity: 
+// Time complexity:
 
 
 
@@ -169,4 +168,3 @@ LinkedList.prototype.removeBefore = function(node) {
 1 1 5 6   (1 -> 1 -> 5 -> 6)
 
  */
-
