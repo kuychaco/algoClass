52eb253f68b03383f3461db83c61c4a4a2164d04
Katrina Uychaco <kuychaco@gmail.com>
2015-11-17 09:35:17 -0800

    Fix bug

--- recursion/paintFill.js
+++ recursion/paintFill.js
@@ -8,8 +8,8 @@ function paintFill(screen, point, newColor) {
   var originalColor = screen[point.row][point.column];
   function recurse(row, column) {
     screen[row][column] = newColor;
-    if (screen[row-1][column] === originalColor) recurse(row-1, column);
-    if (screen[row+1][column] === originalColor) recurse(row+1, column);
+    if (screen[row-1] && screen[row-1][column] === originalColor) recurse(row-1, column);
+    if (screen[row+1] && screen[row+1][column] === originalColor) recurse(row+1, column);
     if (screen[row][column-1] === originalColor) recurse(row, column-1);
     if (screen[row][column+1] === originalColor) recurse(row, column+1);
   }
@@ -25,4 +25,4 @@ var screen = [
   [1,1,1,1,3,3,3]
 ];
 
-console.log(paintFill(screen, {row: 2, column: 4}, 5));
+console.log(paintFill(screen, {row: 4, column: 4}, 5));
