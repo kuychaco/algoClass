cb63921d2abe4b162dbcc843f285aa9e4f48890e
strtw <strtw@users.noreply.github.com>
2017-09-07 10:37:23 -0700

    Update recursionIntro.js

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -6,6 +6,6 @@
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
 
-//5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+//5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value by num and returns an array of the values.
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
