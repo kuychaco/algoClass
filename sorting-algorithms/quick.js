a59f696d721174d17f26c4c1e198b6ffd070567c
MujunZ <o.6180339@gmail.com>
2019-05-04 18:12:08 -0400

    quick sort

--- sorting-algorithms/quick.js
+++ sorting-algorithms/quick.js
@@ -36,46 +36,41 @@ not adaptive
 Use cases:
 Quicksort is in place and has low overhead. If a stable sort is not necessary. It has a higher worstcase time complexity than merge sort (if pivot is not in center of array)
 */
-function quicksort(array, lo, hi) {
-  if (lo === undefined) lo = 0;
-  if (hi === undefined) hi = array.length-1;
 
-  if (lo < hi) {
-    // partition array
-    var p = partition(array, lo, hi);
-    console.log('partitioning from', lo, 'to', hi, '=> partition:',  p);
-    // sort subarrays
-    quicksort(array, lo, p-1);
-    quicksort(array, p+1, hi);
-  }
+// ref: https://www.youtube.com/watch?v=SLauY6PpjW4
+function quicksort(array, left = 0, right = array.length-1) {
 
-  // for initial call, return sorted array
-  if (hi-lo === array.length-1) return array;
+  if(left >= right) return;
+  let pvt = array[(left+right)>>1];
+  let idx = partition(array, left, right, pvt);
+  quicksort(array, left, idx-1);
+  quicksort(array, idx, right);
+  return array;
 }
 
 // Lomuto partition scheme
-function partition(arr, lo, hi) {
-  // choose last element as pivot
-  var pivot = arr[hi];
-  // keep track of index to put pivot at
-  var pivotLoc = lo;
-  // iterate through subarray and if element <= pivot, place element before pivotLoc
-  for (var i=lo; i<hi; i++) {
-    if (arr[i] <= pivot) {
-      swap(arr, pivotLoc, i);
-      pivotLoc++;
+function partition(arr, left, right, pivot) {
+  while (left <= right) {
+    while (arr[left] < pivot) {
+      left++;
+    }
+    while (arr[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      swap(arr, left, right);
+      left++;
+      right--;
     }
   }
-  // move pivot to its proper location
-  swap(arr, pivotLoc, hi);
-  return pivotLoc;
+  return left;
 }
 
-function swap (arr, i1, i2) {
-  if (i1 === i2) return;
-  var temp = arr[i1];
-  arr[i1] = arr[i2];
-  arr[i2] = temp;
-  console.log('swapped', arr[i1], arr[i2], 'in', arr);
-  return arr;
+function swap (arr, a, b) {
+  if(a === b) return;
+  arr[a] = arr[a]^arr[b];
+  arr[b] = arr[a]^arr[b];
+  arr[a] = arr[a]^arr[b];
 }
+
+console.log(quicksort([3,2,3,1,2,4,5,5,6]))
